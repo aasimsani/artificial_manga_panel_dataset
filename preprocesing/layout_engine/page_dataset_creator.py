284c1fbe9a52b77bbce0c1b8ce94c46a3d091924
aasimsani <aasimsani05@gmail.com>
2021-04-14 19:40:55 +0530

    Generalized drawing algorithm to n with shifting

--- preprocesing/layout_engine/page_dataset_creator.py
+++ preprocesing/layout_engine/page_dataset_creator.py
@@ -29,25 +29,201 @@ def test_render(dims):
 
     page.show()
 
+def draw_n_shifted(n, topleft, topright, bottomright, bottomleft, horizontal_vertical, shifts=[]):
+
+        # Allow each inital panel to grow to up to 75% of 100/n
+        choice_max = round((100/n)*1.5)
+        choice_min = round((100/n)*0.5)
+        if len(shifts) < 1:
+            for i in range(0, n):
+                shift_choice = np.random.randint(choice_min, choice_max)
+                choice_max = choice_max + ((100/n) - shift_choice)
+                shifts.append(shift_choice)
+        
+        to_add_or_remove = (100 - sum(shifts))/len(shifts)
+
+        normalized_shifts = []
+        for shift in shifts:
+            new_shift = shift + to_add_or_remove
+            normalized_shifts.append(new_shift/100)
+
+        polys = []
+
+        horizontal_vertical = "v"
+        if horizontal_vertical == "h":
+            shift_level = 0.0 
+            for i in range(0, n):
+                if i == 0:
+                    x1y1 = topleft
+                    x2y2 = topright
+                else:
+                   shift_level += normalized_shifts[i-1]
+                   x1y1 = (bottomleft[0], topleft[1] + (bottomleft[1] - topleft[1])*shift_level)
+                   x2y2 = (bottomright[0], topright[1] + (bottomright[1]- topright[1])*shift_level)
+                
+                if i == (n-1):
+                    x3y3 = bottomright
+                    x4y4 = bottomleft
+                else:
+                    next_shift_level = shift_level + normalized_shifts[i]
+                    x3y3 = (bottomright[0], topright[1] + (bottomright[1]- topright[1])*next_shift_level)
+                    x4y4 = (bottomleft[0], topleft[1] + (bottomleft[1] - topleft[1])*next_shift_level)
+
+                poly = (x1y1, x2y2, x3y3, x4y4, x1y1)
+                polys.append(poly)
+        
+        if horizontal_vertical == "v":
+            shift_level = 0.0 
+            for i in range(0, n):
+                if i == 0:
+                    x1y1 = topleft
+                    x4y4 = bottomleft 
+                else:
+                    shift_level += normalized_shifts[i-1]
+                    x1y1 = (topleft[0] + (topright[0] - topleft[0])*shift_level, topright[1])
+                    x4y4 = (bottomleft[0] + (bottomright[0] - bottomleft[0])*shift_level, bottomright[1])
+
+                if i == (n-1):
+                    x2y2 = topright
+                    x3y3 = bottomright
+                else:
+                    next_shift_level = shift_level + normalized_shifts[i]
+                    x2y2 = (topleft[0] + (topright[0] - topleft[0])*next_shift_level, topright[1])
+                    x3y3 = (bottomleft[0] + (bottomright[0] - bottomleft[0])*next_shift_level, bottomright[1])
+
+                poly = (x1y1, x2y2, x3y3, x4y4, x1y1)
+                polys.append(poly)
+
+        return polys
+
+def draw_n(n, topleft, topright, bottomright, bottomleft, horizontal_vertical):
+
+        polys = []
+        
+        if horizontal_vertical == "h":
+            for i in range(0, n):
+                if i == 0:
+                    x1y1 = topleft
+                    x2y2 = topright
+                else:
+                   x1y1 = (bottomleft[0], topleft[1] + (bottomleft[1] - topleft[1])*(i/n))
+                   x2y2 = (bottomright[0], topright[1] + (bottomright[1]- topright[1])*(i/n))
+                
+                if i == (n-1):
+                    x3y3 = bottomright
+                    x4y4 = bottomleft
+                else:
+                    x3y3 = (bottomright[0], topright[1] + (bottomright[1]- topright[1])*((i+1)/n))
+                    x4y4 = (bottomleft[0], topleft[1] + (bottomleft[1] - topleft[1])*((i+1)/n))
+
+                poly = (x1y1, x2y2, x3y3, x4y4, x1y1)
+                polys.append(poly)
+        
+        if horizontal_vertical == "v":
+            for i in range(0, n):
+
+                if i == 0:
+                    x1y1 = topleft
+                    x4y4 = bottomleft 
+                else:
+                    x1y1 = (topleft[0] + (topright[0] - topleft[0])*(i/n), topright[1])
+                    x4y4 = (bottomleft[0] + (bottomright[0] - bottomleft[0])*(i/n), bottomright[1])
+
+                if i == (n-1):
+                    x2y2 = topright
+                    x3y3 = bottomright
+                else:
+                    x2y2 = (topleft[0] + (topright[0] - topleft[0])*((i+1)/n), topright[1])
+                    x3y3 = (bottomleft[0] + (bottomright[0] - bottomleft[0])*((i+1)/n), bottomright[1])
+
+                poly = (x1y1, x2y2, x3y3, x4y4, x1y1)
+                polys.append(poly)
+
+        return polys
+
+def draw_four(topleft, topright, bottomright, bottomleft, horizontal_vertical):
+
+    if horizontal_vertical == "h":
+        r1x1y1 = topleft
+        r1x2y2 = topright
+        r1x3y3 = (bottomright[0], topright[1] + (bottomright[1]- topright[1])/4)
+        r1x4y4 = (bottomleft[0], topleft[1] + (bottomleft[1] - topleft[1])/4)
+
+        poly1 = (r1x1y1, r1x2y2, r1x3y3, r1x4y4, r1x1y1)
+
+        r2x1y1 = (bottomleft[0], topleft[1] + (bottomleft[1] - topleft[1])/4)
+        r2x2y2 = (bottomright[0], topright[1] + (bottomright[1]- topright[1])/4)
+        r2x3y3 = (bottomright[0], topright[1] + (bottomright[1]- topright[1])*(2/4))
+        r2x4y4 = (bottomleft[0], topleft[1] + (bottomleft[1] - topleft[1])*(2/4))
+
+        poly2 = (r2x1y1, r2x2y2, r2x3y3, r2x4y4, r2x1y1)
+
+        r3x1y1 = (bottomleft[0], topleft[1] + (bottomleft[1] - topleft[1])*(2/4))
+        r3x2y2 = (bottomright[0], topright[1] + (bottomright[1]- topright[1])*(2/4))
+        r3x3y3 = (bottomright[0], topright[1] + (bottomright[1]- topright[1])*(3/4))
+        r3x4y4 = (bottomleft[0], topleft[1] + (bottomleft[1] - topleft[1])*(3/4))
+
+        poly3 = (r3x1y1, r3x2y2, r3x3y3, r3x4y4, r3x1y1)
+
+        r4x1y1 = (bottomleft[0], topleft[1] + (bottomleft[1] - topleft[1])*(3/4))
+        r4x2y2 = (bottomright[0], topright[1] + (bottomright[1]- topright[1])*(3/4))
+        r4x3y3 = bottomright
+        r4x4y4 = bottomleft 
+
+        poly4 = (r4x1y1, r4x2y2, r4x3y3, r4x4y4, r4x1y1)
+
+    
+    if horizontal_vertical == "v":
+        r1x1y1 = topleft
+        r1x2y2 = (topright[0]/4, topright[1])
+        r1x3y3 = (bottomright[0]/4, bottomright[1])
+        r1x4y4 = bottomleft
+
+        poly1 = (r1x1y1, r1x2y2, r1x3y3, r1x4y4, r1x1y1)
+
+        r2x1y1 = (topright[0]/4, topright[1])
+        r2x2y2 = (topright[0]*(2/4), topright[1])
+        r2x3y3 = (bottomright[0]*(2/4), bottomright[1])
+        r2x4y4 = (bottomright[0]/4, bottomright[1])
+
+        poly2 = (r2x1y1, r2x2y2, r2x3y3, r2x4y4, r2x1y1)
+
+        r3x1y1 = (topright[0]*(2/4), topright[1])
+        r3x2y2 = (topright[0]*(3/4), topright[1])
+        r3x3y3 = (bottomright[0]*(3/4), bottomright[1])
+        r3x4y4 = (bottomright[0]*(2/4), bottomright[1])
+
+        poly3 = (r3x1y1, r3x2y2, r3x3y3, r3x4y4, r3x1y1)
+
+        r4x1y1 = (topright[0]*(3/4), topright[1])
+        r4x2y2 = topright
+        r4x3y3 = bottomright
+        r4x4y4 = (bottomright[0]*(3/4), bottomright[1])
+
+        poly4 = (r4x1y1, r4x2y2, r4x3y3, r4x4y4, r4x1y1)
+    
+    return poly1, poly2, poly3, poly4
+
+# TODO: Add a shifting version of this
 def draw_three(topleft, topright, bottomright, bottomleft, horizontal_vertical):
 
     if horizontal_vertical == "h":
         r1x1y1 = topleft
         r1x2y2 = topright
-        r1x3y3 = (bottomright[0], bottomright[1]/3)
-        r1x4y4 = (bottomleft[0], bottomleft[1]/3)
+        r1x3y3 = (bottomright[0], topright[1] + (bottomright[1]- topright[1])/3)
+        r1x4y4 = (bottomleft[0], topleft[1] + (bottomleft[1] - topleft[1])/3)
 
         poly1 = (r1x1y1, r1x2y2, r1x3y3, r1x4y4, r1x1y1)
 
-        r2x1y1 = (bottomleft[0], bottomleft[1]/3)
-        r2x2y2 = (bottomright[0], bottomright[1]/3)
-        r2x3y3 = (bottomright[0], bottomright[1]*(2/3))
-        r2x4y4 = (bottomleft[0], bottomleft[1]*(2/3))
+        r2x1y1 = (bottomleft[0], topleft[1] + (bottomleft[1] - topleft[1])/3)
+        r2x2y2 = (bottomright[0], topright[1] + (bottomright[1]- topright[1])/3)
+        r2x3y3 = (bottomright[0], topright[1] + (bottomright[1]- topright[1])*(2/3))
+        r2x4y4 = (bottomleft[0], topleft[1] + (bottomleft[1] - topleft[1])*(2/3))
 
         poly2 = (r2x1y1, r2x2y2, r2x3y3, r2x4y4, r2x1y1)
 
-        r3x1y1 = (bottomleft[0], bottomleft[1]*(2/3))
-        r3x2y2 = (bottomright[0], bottomright[1]*(2/3))
+        r3x1y1 = (bottomleft[0], topleft[1] + (bottomleft[1] - topleft[1])*(2/3))
+        r3x2y2 = (bottomright[0], topright[1] + (bottomright[1]- topright[1])*(2/3))
         r3x3y3 = bottomright
         r3x4y4 = bottomleft 
 
@@ -283,15 +459,15 @@ def create_page_metadata():
         if num_panels == 4:
 
             ret_list = []
-            # Draw two rectangles 
             horizontal_vertical = np.random.choice(["h", "v"])
-            p1, p2 = draw_two_shifted(*dims, horizontal_vertical, shift=0.5)
-
-            next_div = invert_for_next(horizontal_vertical)
-
             type_choice = np.random.choice(["eq", "uneq", "div", "trip"]) 
+
+            # Draw two rectangles 
             if type_choice == "eq":
-            # Divide each into 2 rectangles equally
+                p1, p2 = draw_two_shifted(*dims, horizontal_vertical, shift=0.5)
+                next_div = invert_for_next(horizontal_vertical)
+
+                # Divide each into 2 rectangles equally
                 shift_min = 25
                 shift_max = 75
                 shift = np.random.randint(shift_min, shift_max)
@@ -303,11 +479,16 @@ def create_page_metadata():
 
             # Divide each into 2 rectangles unequally
             elif type_choice == "uneq":
+                p1, p2 = draw_two_shifted(*dims, horizontal_vertical, shift=0.5)
+                next_div = invert_for_next(horizontal_vertical)
+
                 p3, p4 = draw_two_shifted(*p1[0:4], next_div)
                 p5, p6 = draw_two_shifted(*p2[0:4], next_div)
                 ret_list = [p3, p4, p5, p6]
 
             elif type_choice == "div":
+                p1, p2 = draw_two_shifted(*dims, horizontal_vertical, shift=0.5)
+                next_div = invert_for_next(horizontal_vertical)
                 pick_one = np.random.random()
                 # Pick one and divide into 2 rectangles
                 choice1, left_choices1 = choose_and_return([p1, p2]) 
@@ -322,7 +503,6 @@ def create_page_metadata():
             
             # Draw three rectangles
             elif type_choice == "trip":
-                horizontal_vertical = np.random.choice(["h", "v"])
                 p1, p2, p3 = draw_three(*dims, horizontal_vertical)
 
                 # Pick one and divide it into two
@@ -340,15 +520,16 @@ def create_page_metadata():
             ret_list = []
             # Draw two rectangles 
             horizontal_vertical = np.random.choice(["h", "v"])
-            p1, p2 = draw_two_shifted(*dims, horizontal_vertical, shift=0.5)
             
-            type_choice = np.random.choice(["eq", "uneq", "div", "twothree"])
-            type_choice = "div"
+            type_choice = np.random.choice(["eq", "uneq", "div", "2-(2+3)", "3-(2+2+1)", "4-(2+(3x1))"])
+            type_choice = "4-(2+(3x1))"
             if type_choice == "eq" or type_choice == "uneq":
+
+                p1, p2 = draw_two_shifted(*dims, horizontal_vertical, shift=0.5)
+                next_div = invert_for_next(horizontal_vertical)
 
                 # Pick one and divide it into two then
                 choice, left_choices = choose_and_return([p1, p2])
-                next_div = invert_for_next(horizontal_vertical)
                 p3, p4 = draw_two_shifted(*choice[0:4], next_div)                    
 
                 # Divide each into 2 rectangles equally
@@ -369,8 +550,10 @@ def create_page_metadata():
 
             # Draw two rectangles
             elif type_choice == "div":
-                # Divide both equally
+                p1, p2 = draw_two_shifted(*dims, horizontal_vertical, shift=0.5)
                 next_div = invert_for_next(horizontal_vertical)
+
+                # Divide both equally
                 p3, p4 = draw_two_shifted(*p1[0:4], next_div)
                 p5, p6 = draw_two_shifted(*p2[0:4], next_div)
 
@@ -381,19 +564,57 @@ def create_page_metadata():
 
                 ret_list = left_choices + [p7, p8]
         
-
-            # elif type_choice == "twothree":
-
-            test_render(ret_list) 
             # Draw two rectangles
+            elif type_choice == "2-(2+3)":
+                
+                p1, p2 = draw_two_shifted(*dims, horizontal_vertical, shift=0.5)
+                next_div = invert_for_next(horizontal_vertical)
+
+                # Pick which one gets 2 and which gets 3 
+                choice, left_choices = choose_and_return([p1, p2])
+                other = left_choices[0]
+
                 # Divide one into 2
+                next_div = invert_for_next(horizontal_vertical)
+                p3, p4 = draw_two_shifted(*choice[0:4], next_div)
+
                 # Divide other into 3
+                p5, p6, p7 = draw_three(*other[0:4], next_div)
+
+                ret_list = [p3, p4, p5, p6, p7]
 
             # Draw 3 rectangles (horizontally or vertically)
+            elif type_choice == "3-(2+2+1)":
+
+                p1, p2, p3 = draw_three(*dims, horizontal_vertical)
+                next_div = invert_for_next(horizontal_vertical)
+
+                choice1, left_choices1 = choose_and_return([p1, p2, p3])
+                choice2, left_choices2 = choose_and_return(left_choices1)
+
                 # Pick two and divide each into two
+                p4, p5 = draw_two_shifted(*choice1[0:4], next_div)
+                p6, p7 = draw_two_shifted(*choice2[0:4], next_div)
+
+                ret_list = left_choices2 + [p4, p5, p6, p7]
             
             # Draw 4 rectangles vertically
-                # Pick one and divide into two
+            elif type_choice == "4-(2+(3x1))":
+                # p1, p2, p3, p4 = draw_four(*dims, horizontal_vertical)
+
+                # # Pick one and divide into two
+                # choice, left_choices = choose_and_return([p1, p2, p3, p4])
+                # next_div = invert_for_next(horizontal_vertical)
+                # p5, p6 = draw_two_shifted(*choice[0:4], next_div)
+
+                # ret_list = left_choices + [p5, p6]
+
+                # ret_list = draw_n(4, *dims, horizontal_vertical)
+                ret_list = draw_n_shifted(2, *dims, horizontal_vertical)
+
+            test_render(ret_list) 
+
+           
 
             pass
 
