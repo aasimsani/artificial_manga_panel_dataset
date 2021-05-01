5ef4fe0ce54c74ee58f5f6111cfdd31d7a695887
aasimsani <aasimsani05@gmail.com>
2021-05-01 21:59:56 +0530

    fixed compress and decompress

--- utils/compress.py
+++ utils/compress.py
@@ -3,7 +3,14 @@ import os
 print("Creating zip")
 zip_name = "db_illustrations_bw.zip"
 zip_folder = "db_illustrations_bw"
-os.system('cd datasets/image_dataset; zip -r -q %s %s' % (zip_name, zip_folder))
+if not os.path.isfile("dataset/image_dataset/"+zip_name):
+    os.system('cd datasets/image_dataset; zip -r -q %s %s' % (zip_name, zip_folder))
+else:
+    print("Zip file already exists")
 
-print("Removing folder of zip")
-os.system("cd datasets/image_dataset; rm -r %s" % zip_folder)
+print("Please confirm removal the folder of the zip now:")
+inp = input("y/n")
+if inp.lower() = "y":
+    os.system("cd datasets/image_dataset; rm -r %s" % zip_folder)
+else:
+    print("You have chosen not to remove the zip folder")
--- utils/decompress.py
+++ utils/decompress.py
@@ -5,5 +5,9 @@ print("Decompressing images")
 zip_file = "db_illustrations_bw.zip"
 os.system("cd datasets/image_dataset; unzip %s" % zip_file)
 
-print("Removing zip of images")
-os.system("cd datasets/image_dataset; rm %s" % zip_file)
+print("Please confirm removal of the zip:")
+inp = input("y/n")
+if inp.lower() = "y":
+    os.system("cd datasets/image_dataset; rm %s" % zip_file)
+else:
+    print("You have chosen not to remove the zip folder")
