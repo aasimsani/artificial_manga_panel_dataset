90216ccf7544c8504d21a3c2ee11217732a74f70
aasimsani <aasimsani05@gmail.com>
2021-05-01 20:05:52 +0530

    Working on adding comments and fixing unzipping of image dataset

--- scraping/download_images.py
+++ scraping/download_images.py
@@ -5,11 +5,12 @@ def download_db_illustrations():
     """
     Downloads the Tagged Anime Illustrations Kaggle dataset
     """
-    
+
     kaggle_json = "config/kaggle.json"
     if not os.path.isfile(kaggle_json):
         print("Please create a config folder in this directory and add your kaggle credentials")
         return
+        
     zip_file = "datasets/image_dataset/tagged-anime-illustrations.zip"
     if not os.path.isfile(zip_file):
         os.environ['KAGGLE_CONFIG_DIR'] = "config/" 
@@ -22,7 +23,12 @@ def download_db_illustrations():
         api.dataset_download_files(dataset, path="datasets/image_dataset", quiet=False, unzip=False)
 
     print("Finished downloading now unzipping")
-    os.system("unzip "+zip_file +" datasets/image_dataset/tagged-anime-illustrations/")
+    output_dir = "datasets/image_dataset/tagged-anime-illustrations/"
+
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
+
+    os.system("unzip -o "+zip_file +" -d "+output_dir)
     print("Finished unzipping")
 
 def download_speech_bubbles():
